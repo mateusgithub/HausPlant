5705397f40eb55f8e6974fca8487c3ff156d6bb8
s187064 <s187064@LI019>
2017-09-12 20:36:19 -0300

    Adicionando criação de prancheta teste

--- HausPlant/src/tk/hausplant/HausPlant.java
+++ HausPlant/src/tk/hausplant/HausPlant.java
@@ -1,8 +1,12 @@
 package tk.hausplant;
 
+import java.awt.Color;
 import spacedrawboard.visualization.Drawboard;
 import spacedrawboard.visualization.Visualization;
 import tk.hausplant.dao.UserDAO;
+import tk.hausplant.model.Planta;
+import tk.hausplant.view.Renderizador2DPlanta;
+import tk.hausplant.view.Prancheta;
 
 /**
  * Main HausPlant class
@@ -18,10 +22,17 @@ public class HausPlant {
 
         new Visualization("Hello World!", 700, 550, new Drawboard()).showWindow();
 
+        Planta planta = new Planta();
+        
+        Prancheta prancheta = new Prancheta(planta, Color.white);
+        prancheta.showWindow();
+
+        /*
         UserDAO userController = new UserDAO();
         userController.getUsers().forEach(u -> {
             System.out.println("Id: " + u.getId() + " Name: " + u.getName());
         });
+         */
     }
 
 }
