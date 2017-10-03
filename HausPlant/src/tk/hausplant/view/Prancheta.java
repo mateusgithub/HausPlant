5bdf8b7c3f099933cd8133bce41cc846472b1a3c
sergiodeveloper <sergio-fh@hotmail.com>
2017-10-03 00:33:39 -0300

    Adicionando comentário na classe

--- HausPlant/src/tk/hausplant/view/Prancheta.java
+++ HausPlant/src/tk/hausplant/view/Prancheta.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tk.hausplant.view;
 
 import java.awt.Color;
 import tk.hausplant.model.Planta;
 
 /**
- *
- * @author sergio
+ * Janela onde serão exibidos controles para manipulação da Planta
  */
 public class Prancheta extends javax.swing.JFrame {
 
