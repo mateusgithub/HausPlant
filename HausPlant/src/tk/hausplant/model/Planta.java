2073d6732a59a0f827f384915cec0e094c704c74
sergiodeveloper <sergio-fh@hotmail.com>
2017-10-03 00:12:14 -0300

    Adicionando comentário na classe

--- HausPlant/src/tk/hausplant/model/Planta.java
+++ HausPlant/src/tk/hausplant/model/Planta.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tk.hausplant.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
- * @author sergio
+ * Planta da casa que poderá conter paredes, móveis e telhados
  */
 public class Planta {
 
