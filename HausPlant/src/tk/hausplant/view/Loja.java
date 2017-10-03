b3ca5ea0c95f9ae77b37c2ff5a9e39de5939bb16
sergiodeveloper <sergio-fh@hotmail.com>
2017-10-03 00:23:12 -0300

    Adicionando comentário na classe

--- HausPlant/src/tk/hausplant/view/Loja.java
+++ HausPlant/src/tk/hausplant/view/Loja.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tk.hausplant.view;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.text.ParseException;
@@ -13,8 +7,7 @@ import java.util.ArrayList;
 import javax.swing.BoxLayout;
 
 /**
- *
- * @author sergio
+ * Onde estarão disponíveis os objetos que poderão ser colocados na planta
  */
 public class Loja extends javax.swing.JFrame {
 
@@ -23,8 +16,8 @@ public class Loja extends javax.swing.JFrame {
      *
      * @param indiceItens Caminho do arquivo contendo lista de móveis presentes
      * na loja
-     * @throws java.io.IOException
-     * @throws java.text.ParseException
+     * @throws IOException
+     * @throws ParseException
      */
     public Loja(Path indiceItens) throws IOException, ParseException {
         initComponents();
