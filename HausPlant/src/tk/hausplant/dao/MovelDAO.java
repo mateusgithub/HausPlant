9b9a97170f0f350962ee108238512a9c056d7eb9
sergiodeveloper <sergio-fh@hotmail.com>
2017-10-04 20:53:44 -0300

    Passando método de acesso de dados para classe DAO

---
+++ HausPlant/src/tk/hausplant/dao/MovelDAO.java
@@ -0,0 +1,82 @@
+package tk.hausplant.dao;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import spacedrawboard.resource.Material;
+import spacedrawboard.resource.Mesh;
+import spacedrawboard.resource.Triangle;
+import spacedrawboard.resource.Vector3D;
+import tk.hausplant.model.Movel;
+
+/**
+ * Responsável por manipular arquivos relacionados à Movel
+ */
+public class MovelDAO {
+
+    public static Movel carregarMovel(Path caminho) throws IOException, ParseException {
+
+        // Ler conteúdo do arquivo
+        String source = new String(Files.readAllBytes(caminho));
+
+        JSONArray triangulosJson;
+
+        JSONObject cor;
+
+        try {
+            JSONObject objetoJson = new JSONObject(source);
+            triangulosJson = objetoJson.getJSONArray("triangulos");
+            cor = objetoJson.getJSONObject("cor");
+        } catch (JSONException exception) {
+            throw new ParseException("Arquivo de modelo 3D inválido", -1);
+        }
+
+        Mesh forma = new Mesh();
+
+        // Construir móvel a partir dos triângulos
+        for (int i = 0; i < triangulosJson.length(); i++) {
+            try {
+                JSONArray t = triangulosJson.getJSONArray(i);
+
+                if (t.length() < 9) {
+                    // Ignorar triangulo inválido
+                    continue;
+                }
+
+                double[] v = new double[9];
+                for (int j = 0; j < 9; j++) {
+                    v[j] = t.getDouble(j);
+                }
+
+                Triangle novoTriangulo = new Triangle(
+                        new Vector3D(v[0], v[1], v[2]),
+                        new Vector3D(v[3], v[4], v[5]),
+                        new Vector3D(v[6], v[7], v[8])
+                );
+                forma.addTriangle(novoTriangulo);
+            } catch (JSONException exception) {
+                // Ignorar triangulo inválido
+            }
+        }
+
+        Material material;
+
+        try {
+            int r = cor.getInt("r"),
+                    g = cor.getInt("g"),
+                    b = cor.getInt("b");
+
+            material = new Material(new Color(r, g, b));
+
+        } catch (JSONException exception) {
+            material = new Material(Movel.COR_PADRAO);
+        }
+
+        return new Movel(forma, material);
+    }
+}
