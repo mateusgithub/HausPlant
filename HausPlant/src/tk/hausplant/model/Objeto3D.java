9a27dc034f157e4aa760dd1ab9977495695b72b9
Ramon Ramalho <lenovo@lenovo-PC>
2017-10-09 20:42:41 -0300

    adicionando atributos ao objeto 3d

--- HausPlant/src/tk/hausplant/model/Objeto3D.java
+++ HausPlant/src/tk/hausplant/model/Objeto3D.java
@@ -4,5 +4,31 @@ package tk.hausplant.model;
  * Objeto tridimensional genérico
  */
 public abstract class Objeto3D {
-    
+
+    protected float x, y, z;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
 }
