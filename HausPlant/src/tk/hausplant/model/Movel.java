5df35b9ad619cd584718745a45b3e99c83ffa094
Ramon Ramalho <lenovo@lenovo-PC>
2017-10-09 20:26:52 -0300

    criando extends de objeto 3d e criando atributo para Model ao invés de estender

--- HausPlant/src/tk/hausplant/model/Movel.java
+++ HausPlant/src/tk/hausplant/model/Movel.java
@@ -8,12 +8,13 @@ import spacedrawboard.resource.Model;
 /**
  * Objeto tridimensional que pode ser colocado na planta
  */
-public class Movel extends Model implements Objeto3D {
+public class Movel extends Objeto3D {
 
     public static final Color COR_PADRAO = Color.gray;
+    private final Model modelo;
 
     public Movel(Mesh forma, Material material) {
-        super(forma, material);
+        modelo = new Model(forma, material);
     }
 
 }
