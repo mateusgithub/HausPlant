951128a54d780266a38cac6f8d6636bca4c47108
ramonramalho <ramonramalh@hotmail.com>
2017-10-24 21:58:40 -0200

    Feature carregar stl
    * Criando método carregarMovelSTL para importar como objeto um Movel e criando main para testar a funcionalidade
    
    * Criando método getModelo para obter o modelo 3d de um móvel
    
    * Implementando salvar planta

--- HausPlant/src/tk/hausplant/dao/MovelDAO.java
+++ HausPlant/src/tk/hausplant/dao/MovelDAO.java
@@ -1,18 +1,27 @@
 package tk.hausplant.dao;
 
 import java.awt.Color;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import spacedrawboard.resource.Material;
 import spacedrawboard.resource.Mesh;
+import spacedrawboard.resource.Model;
 import spacedrawboard.resource.Triangle;
 import spacedrawboard.resource.Vector3D;
+import spacedrawboard.visualization.Drawboard;
+import spacedrawboard.visualization.Visualization;
 import tk.hausplant.model.Movel;
+import tk.hausplant.model.Parede;
 
 /**
  * Responsável por manipular arquivos relacionados à Movel
@@ -79,4 +88,93 @@ public class MovelDAO {
 
         return new Movel(forma, material);
     }
+
+    public static Movel carregarMovelSTL(Path caminho) throws IOException, ParseException {
+        Mesh forma = new Mesh();
+        BufferedReader source;
+        try {
+            source = Files.newBufferedReader(caminho,
+                    StandardCharsets.ISO_8859_1);
+            String line = source.readLine();
+
+            // Construir móvel a partir dos triângulos
+            while (true) {
+
+                while (line != null && !line.trim().startsWith("outer loop")) {
+                    line = source.readLine();
+
+                }
+
+                if (line == null) {
+                    break;
+                }
+
+                // valores do triangulo
+                double v[] = new double[9];
+
+                // TRIANGULO
+                for (int i = 0; i < 3; i++) {
+                    line = source.readLine();
+
+                    if (line == null) {
+                        throw new ParseException("Fim de arquivo inesperado", -1);
+                    }
+                    line = line.trim().replaceFirst("vertex ", "");
+
+                    String[] verticeStrings = line.split("\\s+", 3);
+
+                    if (verticeStrings.length < 3) {
+                        //vertice inaválido
+                        throw new ParseException("Arquivo corrompido", -1);
+                    }
+                    v[3 * i + 0] = Double.valueOf(verticeStrings[0]);
+                    v[3 * i + 1] = Double.valueOf(verticeStrings[1]);
+                    v[3 * i + 2] = Double.valueOf(verticeStrings[2]);
+
+                }
+
+                Triangle novoTriangulo = new Triangle(
+                        new Vector3D(v[0], v[1], v[2]),
+                        new Vector3D(v[3], v[4], v[5]),
+                        new Vector3D(v[6], v[7], v[8])
+                );
+                forma.addTriangle(novoTriangulo);
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PlantaDAO.class.getName()).
+                    log(Level.SEVERE, "Falha ao ler arquivo planta csv", ex);
+        }
+
+        Material material;
+
+        int r = 191,
+                g = 124,
+                b = 57;
+
+        material = new Material(new Color(r, g, b));
+
+        return new Movel(forma, material);
+    }
+
+    public static void main(String args[]) {
+        Drawboard d = new Drawboard();
+
+        Movel m = null;
+        try {
+            m = MovelDAO.carregarMovelSTL(Paths.get("modeloteste.stl"));
+        } catch (IOException ex) {
+            Logger.getLogger(MovelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        } catch (ParseException ex) {
+            Logger.getLogger(MovelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+
+        d.addModel(m.getModelo());
+
+        Visualization v = new Visualization("Movel", 750, 450, d);
+        v.showWindow();
+    }
+
 }
