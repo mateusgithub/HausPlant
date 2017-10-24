07e77010272dafb308307b54fe3ff973071fae52
Mateus Tanaka <mateusht@ciandt.com>
2017-10-24 20:16:57 -0200

    Implementando salvar planta

--- HausPlant/src/tk/hausplant/controller/PlantaController.java
+++ HausPlant/src/tk/hausplant/controller/PlantaController.java
@@ -5,6 +5,7 @@
  */
 package tk.hausplant.controller;
 
+import java.util.logging.Logger;
 import tk.hausplant.dao.PlantaDAO;
 import tk.hausplant.model.Planta;
 
@@ -15,11 +16,18 @@ import tk.hausplant.model.Planta;
  */
 public class PlantaController {
     
+    private static final Logger LOG = Logger.getLogger(PlantaController.class.getName());
     private PlantaDAO plantaDAO;
     
-    public Planta carregarPlanta(){
+    public Planta carregar(){
+        LOG.info("Carregando planta");
         plantaDAO = new PlantaDAO();
         return plantaDAO.carregar();
     }
     
+    public void salvar(Planta planta){
+        LOG.info("Salvando planta");
+        plantaDAO = new PlantaDAO();
+        plantaDAO.salvar(planta);
+    }
 }
