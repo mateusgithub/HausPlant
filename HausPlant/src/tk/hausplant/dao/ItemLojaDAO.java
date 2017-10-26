f9d83843d697f88c96be5806d1d0d7db9227ea2c
Ramon Ramalho <lenovo@lenovo-PC>
2017-10-26 20:35:45 -0200

    Criando método ler Itens CSV e main para teste

--- HausPlant/src/tk/hausplant/dao/ItemLojaDAO.java
+++ HausPlant/src/tk/hausplant/dao/ItemLojaDAO.java
@@ -5,16 +5,24 @@
  */
 package tk.hausplant.dao;
 
+import java.io.BufferedReader;
 import java.util.List;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import spacedrawboard.resource.Triangle;
+import spacedrawboard.resource.Vector3D;
 import tk.hausplant.view.ItemLoja;
+import tk.hausplant.view.Loja;
 
 /**
  *
@@ -63,5 +71,53 @@ public class ItemLojaDAO {
         }
 
         return lista;
+    }
+
+    public static List<ItemLoja> lerItensCSV(Path caminho) throws ParseException, IOException {
+
+        BufferedReader source = null;
+        List<ItemLoja> lista = new ArrayList<>();
+
+        source = Files.newBufferedReader(caminho,
+                StandardCharsets.UTF_8);
+
+        String line = source.readLine();
+
+        while ((line = source.readLine()) != null) {
+
+            if (line.trim().isEmpty()) {
+                continue;
+            }
+
+            String[] variaveis = line.trim().split("\\s*,\\s*", 3);
+            if (variaveis.length < 3) {
+                continue;
+            }
+            ItemLoja novoItem = new ItemLoja(variaveis[0], variaveis[1], variaveis[2]);
+            lista.add(novoItem);
+
+        }
+
+        return lista;
+    }
+
+    public static void main(String[] args) throws ParseException, IOException {
+        /*
+        List<ItemLoja> lista=null;
+        try {
+            lista = ItemLojaDAO.lerItensDAO(Paths.get("teste.txt"));
+        } catch (ParseException ex) {
+            Logger.getLogger(ItemLojaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        } catch (IOException ex) {
+            Logger.getLogger(ItemLojaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        for(ItemLoja a : lista){
+            System.out.println(a.getNome()+", " + a.getLocalFoto() + ", " +  a.getLocalModelo() );
+        }
+         */
+        Loja loja = new Loja(Paths.get("teste.txt"));
+        loja.setVisible(true);
     }
 }
