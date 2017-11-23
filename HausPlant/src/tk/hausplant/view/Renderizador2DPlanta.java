5f5f219cbeba0b10c656d2a9c48e6d7f40d0d31b
s187064 <s187064@LI012>
2017-11-22 22:57:05 -0200

    Adicionando classe para operação assíncrona de carregamento

--- HausPlant/src/tk/hausplant/view/Renderizador2DPlanta.java
+++ HausPlant/src/tk/hausplant/view/Renderizador2DPlanta.java
@@ -11,6 +11,7 @@ import java.awt.Graphics;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import javax.swing.JPanel;
+import tk.hausplant.model.Movel;
 import tk.hausplant.model.Planta;
 import tk.hausplant.model.Parede;
 
@@ -58,8 +59,12 @@ public class Renderizador2DPlanta extends JPanel {
     public void atualizar() {
         clear();
 
-        for (Parede wall : planta.getParedes()) {
-            wall.desenhar2DEm(getGraphics());
+        for (Parede parede : planta.getParedes()) {
+            parede.desenhar2DEm(getGraphics());
+        }
+
+        for (Movel movel : planta.getMoveis()) {
+            movel.desenhar2DEm(getGraphics());
         }
     }
 }
