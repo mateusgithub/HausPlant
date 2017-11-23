1c856ffa07dced9b41985e2ba575a95c92599acf
s187064 <s187064@LI003>
2017-11-23 21:15:07 -0200

    Implementando método desenhar3DEm

--- HausPlant/src/tk/hausplant/model/Parede.java
+++ HausPlant/src/tk/hausplant/model/Parede.java
@@ -11,6 +11,13 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.io.Serializable;
+import spacedrawboard.resource.Material;
+import spacedrawboard.resource.Mesh;
+import spacedrawboard.resource.Model;
+import spacedrawboard.resource.Triangle;
+import spacedrawboard.resource.Vector3D;
+import spacedrawboard.visualization.Drawboard;
+import tk.hausplant.view.Prancheta;
 
 public class Parede extends Objeto3D implements Serializable, Desenhavel {
 
@@ -106,6 +113,76 @@ public class Parede extends Objeto3D implements Serializable, Desenhavel {
         graficos2d.fillPolygon(polygon);
         //g.setColor(Color.black);
         //g.drawPolygon(polygon);
+    }
+
+    @Override
+    public void desenhar3DEm(Drawboard drawboard) {
+        // Pixel por metro
+        double pxMetro = Prancheta.PIXELS_POR_METRO;
+
+        Material materialParede = new Material(Parede.COR_PADRAO);
+
+        // Cantos da parede
+        Vector3D A = new Vector3D(getA().x, -getA().y, 0);
+        Vector3D B = new Vector3D(getB().x, -getB().y, 0);
+
+        // Ajustar escalas dos cantos (converter de pixel para metro)
+        A = A.timesScalar(1 / pxMetro);
+        B = B.timesScalar(1 / pxMetro);
+
+        // Cantos superiores da parede
+        Vector3D Ah = A.clone();
+        Vector3D Bh = B.clone();
+        Ah.z = Bh.z = Parede.ALTURA_PADRAO;
+
+        // Vetores para dar largura à parede
+        Vetor2D paralelo = new Vetor2D(A.x, A.y).minus(new Vetor2D(B.x, B.y));
+        Vector3D paral = new Vector3D(paralelo.x, paralelo.y, 0);
+        paral.normalize();
+        Vetor2D perpendicular = paralelo.getOrthogonal();
+        perpendicular.normalize();
+        Vector3D perp = new Vector3D(perpendicular.x, perpendicular.y, 0);
+        perp = perp.timesScalar(Parede.LARGURA_PADRAO);
+        Vector3D perpNeg = perp.clone().timesScalar(-1);
+
+        // Formar os triângulos da parede
+        Mesh formaParede = new Mesh();
+
+        // Lados
+        formaParede.addTriangle(new Triangle(A.plus(perp), Ah.plus(perp), Bh.plus(perp)));
+        formaParede.addTriangle(new Triangle(Bh.plus(perp), B.plus(perp), A.plus(perp)));
+
+        formaParede.addTriangle(new Triangle(Bh.plus(perpNeg), Ah.plus(perpNeg), A.plus(perpNeg)));
+        formaParede.addTriangle(new Triangle(A.plus(perpNeg), B.plus(perpNeg), Bh.plus(perpNeg)));
+
+        // Frentes
+        formaParede.addTriangle(new Triangle(A.plus(perpNeg), Ah.plus(perpNeg), Ah.plus(perp)));
+        formaParede.addTriangle(new Triangle(Ah.plus(perp), A.plus(perp), A.plus(perpNeg)));
+
+        formaParede.addTriangle(new Triangle(B.plus(perp), Bh.plus(perp), Bh.plus(perpNeg)));
+        formaParede.addTriangle(new Triangle(Bh.plus(perpNeg), B.plus(perpNeg), B.plus(perp)));
+
+        // Calha
+        Material materialCalha = new Material(new Color(50, 50, 50));
+        Mesh formaCalha = new Mesh();
+
+        Vector3D alturaCalha = new Vector3D(0, 0, 0.1);
+        double c = 1;
+
+        // Cantos inferiores da calha
+        Vector3D c1 = Ah.plus(perpNeg);
+        Vector3D c2 = Ah.plus(perp);
+        Vector3D c3 = Bh.plus(perp);
+        Vector3D c4 = Bh.plus(perpNeg);
+
+        // Face inferior da calha
+        formaCalha.addTriangle(new Triangle(c1, c2, c3));
+        formaCalha.addTriangle(new Triangle(c3, c4, c1));
+
+        Model modeloParede = new Model(formaParede, materialParede);
+        Model modeloCalha = new Model(formaCalha, materialCalha);
+        drawboard.addModel(modeloParede);
+        drawboard.addModel(modeloCalha);
     }
 
 }
